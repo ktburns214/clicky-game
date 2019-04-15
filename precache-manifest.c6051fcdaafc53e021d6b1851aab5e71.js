ed186037bd9fb01e259edb171aeca18b04b52c63
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2019-04-15 00:59:17 -0500

    Updates

---
+++ precache-manifest.c6051fcdaafc53e021d6b1851aab5e71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b44100856353dacce252",
+    "url": "/clicky-game/static/css/main.ad8870a5.chunk.css"
+  },
+  {
+    "revision": "b44100856353dacce252",
+    "url": "/clicky-game/static/js/main.c9995511.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b41b4923864f688d88098d651d4c340a",
+    "url": "/clicky-game/index.html"
+  }
+];
